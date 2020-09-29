32e504e372a581601f72f6a43c6dbd5b788cdefa
Jacob Tellep <jj.tellep@gmail.com>
2020-09-29 00:25:20 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HelloWorld from "./Components/HelloWorld";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <HelloWorld name="World" />
+    </div>
+  );
+}
+
+export default App;
